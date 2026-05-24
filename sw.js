5ff2b8b524e7cfea63a7931222ab3d93d03c2161
yusagi19 <yusagi19@users.noreply.github.com>
2026-05-24 23:46:25 +0900

    Service Workerによる自動更新を追加

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'kondate-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.add('./')));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
